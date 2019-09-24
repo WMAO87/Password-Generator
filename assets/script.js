5139095043f5e5b1370621a24af7c612778e58b8
William Mao <willmao87@gmail.com>
2019-09-24 10:59:27 -0700

    added copy to clipboard not working yet

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,23 @@
 function generateRandomPassword (passwordLength) {
   var outputPassword = "";
-  var allPossibleChars  = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var allPossibleChars  = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~!@#$%^&*()_+-=";
   for (var i = 0; i < passwordLength; i++) {
       outputPassword += allPossibleChars.charAt(Math.floor(Math.random() * allPossibleChars.length));
   }
   return outputPassword;
+}
+
+function myFunction() {
+  var copyText = document.getElementById("myInput");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999);
+  document.execCommand("copy");
+  
+  var tooltip = document.getElementById("myTooltip");
+  tooltip.innerHTML = "Copied: " + copyText.value;
+}
+
+function outFunc() {
+  var tooltip = document.getElementById("myTooltip");
+  tooltip.innerHTML = "Copy to clipboard";
 }
